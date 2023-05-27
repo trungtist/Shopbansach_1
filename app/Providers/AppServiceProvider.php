4c92395488b093a8fa838d68ceba20d99b75f12b
Trung Hoang <trungtit12082001@gmail.com>
2023-05-27 18:34:44 +0700

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') !== 'local') {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
     }
