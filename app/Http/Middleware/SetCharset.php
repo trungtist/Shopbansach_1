23c028f2f3f6c39bd0ba49739cc5333840c80b80
Trung Hoang <trungtit12082001@gmail.com>
2023-05-27 20:43:56 +0700

    update utf8

---
+++ app/Http/Middleware/SetCharset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SetCharset
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $contentType = $response->headers->get('Content-Type');
+        if (strpos($contentType, 'text/html') !== false) {
+            $response->header('Content-Type', 'text/html; charset=UTF-8');
+            $response->header('Character-Encoding', 'UTF-8');
+        }
+
+        return $response;
+    }
+}
