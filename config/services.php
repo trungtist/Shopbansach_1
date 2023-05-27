0d1ebac19e75f376012b81d97c5e9e42bc9a1020
Trung Hoang <trungtit12082001@gmail.com>
2023-05-27 14:56:46 +0700

    update google logn

--- config/services.php
+++ config/services.php
@@ -28,6 +28,10 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ]
-    
+    ],
+    'google' => [
+        'client_id' => env('GG_CLIENT_ID'),
+        'client_secret' => env('GG_CLIENT_SECRET'),
+        'redirect' => 'http://127.0.0.1:8000/callback/google',
+    ],
 ];
